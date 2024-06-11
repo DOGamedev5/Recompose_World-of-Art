2bd4229e0f017c1e20e81c6011d57bb89edac381
Misael Victor Alves Ribeiro <ribeiromisa05@gmail.com>
2024-06-11 19:38:45 -0300

    new logo

--- gameplay/intro/intro.gd
+++ gameplay/intro/intro.gd
@@ -1,12 +1,10 @@
 extends CanvasLayer
 
 onready var menu = preload("res://gameplay/MENU/menu.tscn")
-onready var video = $VideoPlayer
 
 func _input(_event):
 	if Input.is_key_pressed(KEY_ENTER):
 		var _1 = get_tree().change_scene_to(menu)
-		
 
-func _on_VideoPlayer_finished():
+func _on_AnimationPlayer_animation_finished(anim_name):
 	var _1 = get_tree().change_scene_to(menu)
