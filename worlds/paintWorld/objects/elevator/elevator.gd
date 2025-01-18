a2b8c9ea350ef43bb894ebae8651756264a83fec
Misael Victor <ribeiromisa05@gmail.com>
2025-01-17 22:16:19 -0300

    working on elevator

--- worlds/paintWorld/objects/elevator/elevator.gd
+++ worlds/paintWorld/objects/elevator/elevator.gd
@@ -1,8 +1,18 @@
-extends Node2D
+extends KinematicBody2D
 
-export var lineDistante := 299
 export var linePosition := Vector2(-1433, -1697)
 
-func _ready():
-	$Line2D.points[1].y = (linePosition.y - position.y) - $Line2D.position.y
+export var lineStart := Vector2.ZERO
 
+var motion := 0
+var actualMotion := 0
+
+func _physics_process(delta):
+	var pos := to_local(lineStart)
+	
+	$Line2D.points[1].y = pos.y
+	
+	if actualMotion != motion:
+		actualMotion = lerp(actualMotion, motion, 0.3*delta)
+	
+	position.y += actualMotion*delta
