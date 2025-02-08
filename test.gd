bc01eebc9484c6097e9903e878c4b60745f10fae
Misael Victor <ribeiromisa05@gmail.com>
2025-02-08 17:32:56 -0300

    spiral shader effect

---
+++ test.gd
@@ -0,0 +1,15 @@
+extends Sprite
+
+func _ready():
+#	$Tween.interpolate_property(self, "normalOffset", Vector2(-1, -1), Vector2(1, 1), 1)
+	$Tween.interpolate_method(self, "updateOffset", Vector2(-1, -1), Vector2(1, 1), 20)
+	$Tween.start()
+
+func _on_Tween_tween_completed(_object, _key):
+#	$Tween.interpolate_property(self, "normalOffset", Vector2(-1, -1), Vector2(1, 1), 1)
+	$Tween.interpolate_method(self, "updateOffset", Vector2(-1, -1), Vector2(1, 1), 20)
+	$Tween.start()
+
+
+func updateOffset(value):
+	material["shader_param/normalOffset"] = value
